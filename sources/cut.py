603ab4bd6b48d269264578512567274cc6d80c6e
NotHydra <rizky.irswanda115@gmail.com>
2024-05-27 04:02:31 +0800

    fix: remove -1

--- sources/cut.py
+++ sources/cut.py
@@ -1,16 +1,16 @@
 from pydub import AudioSegment 
   
-audioFile = "./sources/sounds/ambulance/raw/utomp3.com - GAWAT Apa Yang Terjadi Banyak Mobil Ambulance Melintas Sitinjau Lauik Hal Yang Ditakuti Terjadi.wav"
-outputFolder = "./sources/sounds/ambulance/test"
+audioFile = "./sources/sounds/traffic-noise/raw/(Done) y2mate.com - Suara lalu lintas kota  suasana jalan raya di sore hari  walking around.mp3"
+outputFolder = "./sources/sounds/traffic-noise/test"
 interval = 5000
-startAt = 314
+startAt = 1
 
-sound = AudioSegment.from_file(audioFile, format="wav") 
+sound = AudioSegment.from_file(audioFile, format="mp3") 
 for index, milisecond in enumerate(range(0, len(sound), interval), startAt):
     if (milisecond + interval) > len(sound):
         break
 
-    sound[milisecond:milisecond + interval - 1].export(f"{outputFolder}/{str(index)}.wav", format="wav")
+    sound[milisecond:(milisecond + interval)].export(f"{outputFolder}/{str(index)}.wav", format="wav")
 
 
   
