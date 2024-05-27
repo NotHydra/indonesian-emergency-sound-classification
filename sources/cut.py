75591f35ef0589ada389a275dfac0c5f059ea117
NotHydra <rizky.irswanda115@gmail.com>
2024-05-27 18:35:45 +0800

    feat: log

--- sources/cut.py
+++ sources/cut.py
@@ -1,9 +1,13 @@
 from pydub import AudioSegment 
   
-audioFile = "./sources/sounds/traffic-noise/raw/(Done) y2mate.com - Suara lalu lintas kota  suasana jalan raya di sore hari  walking around.mp3"
-outputFolder = "./sources/sounds/traffic-noise/test"
+audioFile = "./sources/sounds/ambulance/raw/utomp3.com - - AMBULANCE DRIVER on duty menuju yogyakarta_v144P.mp3"
+outputFolder = "./sources/sounds/ambulance/test"
+
+# audioFile = "./sources/sounds/traffic-noise/raw/SUARA JALAN RAYA MALAM HARI.mp3"
+# outputFolder = "./sources/sounds/traffic-noise/test"
+
 interval = 5000
-startAt = 1
+startAt = 374
 
 sound = AudioSegment.from_file(audioFile, format="mp3") 
 for index, milisecond in enumerate(range(0, len(sound), interval), startAt):
@@ -12,5 +16,4 @@ for index, milisecond in enumerate(range(0, len(sound), interval), startAt):
 
     sound[milisecond:(milisecond + interval)].export(f"{outputFolder}/{str(index)}.wav", format="wav")
 
-
-  
+    print(f"Exported {outputFolder}/{str(index)}.wav")
