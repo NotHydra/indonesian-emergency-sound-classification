14f57f7e0551a07590dd64401ad424ba4e260f9d
NotHydra <rizky.irswanda115@gmail.com>
2024-05-30 05:36:04 +0800

    feat: footer

--- sources/website/src/app/page.tsx
+++ sources/website/src/app/page.tsx
@@ -9,11 +9,7 @@ export default function Home(): JSX.Element {
     const handleFileChange = (event: ChangeEvent<HTMLInputElement>): void => {
         if (event.target.files != null && event.target.files[0] != null) {
             setFile(event.target.files[0]);
-            setFileName(
-                event.target.files[0].name.length > 50
-                    ? event.target.files[0].name.substring(0, 50) + "..."
-                    : event.target.files[0].name
-            );
+            setFileName(event.target.files[0].name.length > 50 ? event.target.files[0].name.substring(0, 50) + "..." : event.target.files[0].name);
         } else {
             setFile(null);
             setFileName("No file chosen");
@@ -33,20 +29,12 @@ export default function Home(): JSX.Element {
                     <div className="">
                         <p className="title has-text-light mb-1">Indonesian Emergency Sound Classification</p>
 
-                        <p className="subtitle has-text-light mb-4">
-                            Upload an audio file below to check for emergency sounds
-                        </p>
+                        <p className="subtitle has-text-light mb-4">Upload an audio file below to check for emergency sounds</p>
 
                         <form onSubmit={handleSubmit}>
                             <div className="file is-light has-name is-right is-fullwidth mb-4">
                                 <label className="file-label">
-                                    <input
-                                        className="file-input"
-                                        type="file"
-                                        name="file"
-                                        accept=".mp3"
-                                        onChange={handleFileChange}
-                                    />
+                                    <input className="file-input" type="file" name="file" accept=".mp3" onChange={handleFileChange} />
 
                                     <span className="file-cta">
                                         <span className="file-icon">
@@ -71,6 +59,20 @@ export default function Home(): JSX.Element {
                     </div>
                 </div>
             </section>
+
+            <footer className="footer">
+                <div className="content has-text-centered">
+                    <div className="is-size-3 mb-2">
+                        <a className="footer-link" href="https://github.com/NotHydra/indonesian-emergency-sound-classification" target="_blank" rel="GitHub">
+                            <i className="fa-brands fa-github"></i>
+                        </a>
+                    </div>
+
+                    <p>
+                        <strong>Copyright © 2024</strong> Indonesian Emergency Sound Classification - Kalimantan's Institute of Technology
+                    </p>
+                </div>
+            </footer>
         </>
     );
 }
