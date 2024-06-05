647cfb6277409563ef3143e29dcb0e9f5f496e87
NotHydra <rizky.irswanda115@gmail.com>
2024-06-05 14:39:31 +0800

    feat: cors

--- sources/server/src/main.py
+++ sources/server/src/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 
 app: FastAPI = FastAPI()
 
-@app.post("/classify")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/api/classify")
 async def upload_file(file: UploadFile = File())->str:
     return file.filename
 
--- sources/server/src/test.py
+++ sources/server/src/test.py
@@ -3,7 +3,7 @@ from fastapi import Response
 
 import requests
 
-url: str = "http://localhost:3001/classify/"
+url: str = "http://localhost:3001/api/classify/"
 files: dict[str, BufferedReader] = {"file": open("1.wav", "rb")}
 response: Response = requests.post(url, files=files)
 
