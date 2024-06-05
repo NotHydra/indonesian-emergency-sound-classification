7d94192c504592737bb6b90df074c54efca8c3df
NotHydra <rizky.irswanda115@gmail.com>
2024-06-06 00:08:38 +0800

    feat(server): pre processing

--- sources/server/src/main.py
+++ sources/server/src/main.py
@@ -1,5 +1,27 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from io import BytesIO
+from types import FunctionType
+
+import librosa
+import numpy as np
+import keras
+
+loaded_model: FunctionType = keras.models.load_model("model.h5")
+
+
+async def load_and_extract_spectrogram(
+    file: UploadFile, n_mels: int = 128, n_fft: int = 1024, hop_length: int = 512
+) -> np.ndarray:
+    y, sr = librosa.load(BytesIO(await file.read()), sr=None)
+
+    mel_spec: np.ndarray = librosa.feature.melspectrogram(
+        y=y, sr=sr, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels
+    )
+    mel_spec_db: np.ndarray = librosa.power_to_db(mel_spec, ref=np.max)
+
+    return mel_spec_db
+
 
 app: FastAPI = FastAPI()
 
@@ -11,9 +33,31 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.post("/api/classify")
-async def upload_file(file: UploadFile = File())->str:
-    return file.filename
+async def upload_file(file: UploadFile = File(...)) -> None:
+    X = []
+    max_time_steps: int = 128
+    spectrogram: np.ndarray = await load_and_extract_spectrogram(file)
+    if spectrogram.shape[1] < max_time_steps:
+        pad_width: int = max_time_steps - spectrogram.shape[1]
+        spectrogram_padded: np.ndarray = np.pad(
+            spectrogram, ((0, 0), (0, pad_width)), mode="constant"
+        )
+
+        X.append(spectrogram_padded)
+    else:
+        X.append(spectrogram[:, :max_time_steps])
+
+    X = np.array(X)
+    X = X[..., np.newaxis]
+
+    temp = loaded_model(X)
+    
+    print(temp)
+    
+    return temp.numpy().tolist()
+
 
 if __name__ == "__main__":
     import uvicorn
--- sources/server/src/test.py
+++ sources/server/src/test.py
@@ -4,7 +4,7 @@ from fastapi import Response
 import requests
 
 url: str = "http://localhost:3001/api/classify/"
-files: dict[str, BufferedReader] = {"file": open("1.wav", "rb")}
+files: dict[str, BufferedReader] = {"file": open("test.wav", "rb")}
 response: Response = requests.post(url, files=files)
 
 print(response.json())
