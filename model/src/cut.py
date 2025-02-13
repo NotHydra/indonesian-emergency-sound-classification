478c5d55391b2e3973b0a74ec20d42303d22cd37
Rizky Irswanda Ramadhana <rizky.irswanda115@gmail.com>
2025-02-13 08:29:25 +0800

    feat(model): venv

--- model/src/cut.py
+++ model/src/cut.py
@@ -1,5 +1,5 @@
-from pydub import AudioSegment 
-  
+from pydub import AudioSegment
+
 audioFile = "./src/sounds/ambulance/raw/utomp3.com - - AMBULANCE DRIVER on duty menuju yogyakarta_v144P.mp3"
 outputFolder = "./src/sounds/ambulance/test"
 
@@ -9,11 +9,13 @@ outputFolder = "./src/sounds/ambulance/test"
 interval = 5000
 startAt = 374
 
-sound = AudioSegment.from_file(audioFile, format="mp3") 
+sound = AudioSegment.from_file(audioFile, format="mp3")
 for index, milisecond in enumerate(range(0, len(sound), interval), startAt):
     if (milisecond + interval) > len(sound):
         break
 
-    sound[milisecond:(milisecond + interval)].export(f"{outputFolder}/{str(index)}.wav", format="wav")
+    sound[milisecond : (milisecond + interval)].export(
+        f"{outputFolder}/{str(index)}.wav", format="wav"
+    )
 
     print(f"Exported {outputFolder}/{str(index)}.wav")
