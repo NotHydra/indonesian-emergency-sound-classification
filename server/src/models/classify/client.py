93eed0593982660eb1ac907c687fdd31f8f20de9
Rizky Irswanda Ramadhana <rizky.irswanda115@gmail.com>
2025-02-14 08:33:58 +0800

    fix(server): history.json is not affected in docker production

--- server/src/models/classify/client.py
+++ server/src/models/classify/client.py
@@ -3,12 +3,7 @@ import os
 import requests
 from requests import Response
 
-path: str = "wav/ambulans/split_49_ambulans full raw 1.wav"
-# path: str = "wav/ambulans/split_207_ambulans full raw 1.wav"
-# path: str = "wav/ambulans/split_469_ambulans full raw 1.wav"
-# path: str = "wav/bukan-ambulans/split_54_bukan ambulans full raw 1.wav"
-# path: str = "wav/bukan-ambulans/split_78_bukan ambulans full raw 1.wav"
-# path: str = "wav/bukan-ambulans/split_567_bukan ambulans full raw 1.wav"
+path: str = "true.wav"
 
 response: Response = requests.post(
     f"http://localhost:{int(os.getenv('PORT'))}/api/classify/",
